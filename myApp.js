8fdaf7dc4c64c564d0b64bdbd2bd2a6b517051c9
Rahul Bhoir <rahulbhoir767676@gmail.com>
2025-11-10 14:36:00 +0530

    up

--- myApp.js
+++ myApp.js
@@ -1,8 +1,10 @@
-let http = require("http");
+const fs = require("fs");
 
-http.createServer(function (req, res) {
-  res.writeHead(200, {"Content-Type" : "text/html"});
-  res.end("Hello World")
-}).listen(8080);
+console.log("Before");
 
-console.log("Server Online")
+fs.readFile("file.txt", "utf8", (err, data) => {
+  if(err) throw err;
+  console.log("File Content : ", data);
+})
+
+console.log("After")
