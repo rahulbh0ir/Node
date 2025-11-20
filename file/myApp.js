87c201eb47ea2b51781c193732f35425688f6d35
Rahul Bhoir <rahulbhoir767676@gmail.com>
2025-11-20 16:34:32 +0530

    up

--- file/myApp.js
+++ file/myApp.js
@@ -1,10 +1,6 @@
 const fs = require("fs");
 
-console.log("Before");
-
 fs.readFile("file.txt", "utf8", (err, data) => {
   if(err) throw err;
-  console.log("File Content : ", data);
+  console.log("File Data : " + data)
 })
-
-console.log("After")
---
+++ module/index.js
@@ -0,0 +1,8 @@
+const addFunc =  require("./add")
+
+const sum = addFunc(3, 5)
+const sum2 = addFunc(66, 6)
+
+console.log("This is index")
+console.log(sum)
+console.log(sum2)
